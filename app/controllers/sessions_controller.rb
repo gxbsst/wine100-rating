118a2e7e6706e6d278e7f6ddf724cdb9322eeca6
Weston Wei <weixuhong@rongyi.com>
2014-06-05 01:56:34 +0800

    add bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:user][:name].blank? && params[:user][:password].blank?
+      redirect_to root_path
+      return
+    end
     @user = User.find_by_name(params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      flash[:error] = "请输入正确的用户名和密码!"
+      @user = User.new
+      flash[:error] = "请输入正确的用户名和密码 Invalid e-mail or password."
       render :new
     end
   end
